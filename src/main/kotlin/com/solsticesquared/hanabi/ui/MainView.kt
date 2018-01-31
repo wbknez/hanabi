2167c3b0d5392615d5f45f74140d8a56625c6947
Will Knez <wbknez.dev@gmail.com>
2018-01-31 10:13:06 -0700

    Add main user interface view.

---
+++ src/main/kotlin/com/solsticesquared/hanabi/ui/MainView.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2018 Will Knez <wbknez.dev@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.solsticesquared.hanabi.ui
+
+import javafx.scene.Parent
+import javafx.scene.layout.BorderPane
+import tornadofx.View
+
+/**
+ * Represents an implementation of [View] that serves as the basis of the
+ * user interface for this project.
+ */
+class MainView : View() {
+
+    override val root: Parent = BorderPane()
+
+    init {
+
+
+    }
+}
