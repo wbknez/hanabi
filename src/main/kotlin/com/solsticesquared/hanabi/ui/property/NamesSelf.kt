2dd7ba5594d5e87ed2078c6a4d4966711fe6cf9a
Will Knez <wbknez.dev@gmail.com>
2018-03-07 05:51:59 -0700

    Add component property that represents a property display name.

---
+++ src/main/kotlin/com/solsticesquared/hanabi/ui/property/NamesSelf.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2018 Will Knez <wbknez.dev@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.solsticesquared.hanabi.ui.property
+
+/**
+ * Represents a [ComponentProperty] that denotes the name of a property the
+ * user will see on a label next to its modifiable component(s).
+ *
+ * @property name
+ *           The display name to use.
+ */
+@MustBeDocumented
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.FIELD)
+@ComponentProperty
+annotation class NamesSelf(
+    val name: String
+)
